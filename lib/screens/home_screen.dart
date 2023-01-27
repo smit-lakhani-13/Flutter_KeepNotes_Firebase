4e3d3642f3e78fee2a1730dc5bc292e5f39d7769
Smit Lakhani <smitlakhani13@gmail.com>
2023-01-27 16:07:16 +0530

    Note Reader Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:keepnotes_firebase/screens/note_reader.dart';
 import 'package:keepnotes_firebase/style/app_style.dart';
 import 'package:keepnotes_firebase/widgets/note_card.dart';
 
@@ -56,7 +57,14 @@ class _HomeScreenState extends State<HomeScreen> {
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                           crossAxisCount: 2),
                       children: snapshot.data!.docs
-                          .map((note) => noteCard(() {}, note))
+                          .map((note) => noteCard(() {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          NoteReaderScreen(note),
+                                    ));
+                              }, note))
                           .toList(),
                     );
                   }
